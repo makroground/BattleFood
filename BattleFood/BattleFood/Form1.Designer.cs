ddb81d5f926211cd15f90389ed797e76939dee8a
makroground <r.bressler@makroground.de>
2015-11-19 19:54:50 +0100

    Erstellen des Projekts Translation from VB.NET

---
+++ BattleFood/BattleFood/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace BattleFood
+{
+    partial class frm_play
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_player2 = new System.Windows.Forms.Panel();
+            this.pbox_player2 = new System.Windows.Forms.PictureBox();
+            this.pbox_health2 = new System.Windows.Forms.PictureBox();
+            this.lbl_name2 = new System.Windows.Forms.Label();
+            this.pnl_player1 = new System.Windows.Forms.Panel();
+            this.pbox_player1 = new System.Windows.Forms.PictureBox();
+            this.pbox_health1 = new System.Windows.Forms.PictureBox();
+            this.lbl_name1 = new System.Windows.Forms.Label();
+            this.pnl_player2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_health2)).BeginInit();
+            this.pnl_player1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_health1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_player2
+            // 
+            this.pnl_player2.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_player2.Controls.Add(this.pbox_player2);
+            this.pnl_player2.Controls.Add(this.pbox_health2);
+            this.pnl_player2.Controls.Add(this.lbl_name2);
+            this.pnl_player2.Location = new System.Drawing.Point(841, 307);
+            this.pnl_player2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnl_player2.Name = "pnl_player2";
+            this.pnl_player2.Size = new System.Drawing.Size(90, 154);
+            this.pnl_player2.TabIndex = 4;
+            // 
+            // pbox_player2
+            // 
+            this.pbox_player2.Location = new System.Drawing.Point(0, 62);
+            this.pbox_player2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_player2.Name = "pbox_player2";
+            this.pbox_player2.Size = new System.Drawing.Size(90, 92);
+            this.pbox_player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox_player2.TabIndex = 2;
+            this.pbox_player2.TabStop = false;
+            // 
+            // pbox_health2
+            // 
+            this.pbox_health2.Location = new System.Drawing.Point(0, 38);
+            this.pbox_health2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_health2.Name = "pbox_health2";
+            this.pbox_health2.Size = new System.Drawing.Size(90, 23);
+            this.pbox_health2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox_health2.TabIndex = 1;
+            this.pbox_health2.TabStop = false;
+            // 
+            // lbl_name2
+            // 
+            this.lbl_name2.Font = new System.Drawing.Font("Berlin Sans FB", 9F, System.Drawing.FontStyle.Bold);
+            this.lbl_name2.ForeColor = System.Drawing.Color.Black;
+            this.lbl_name2.Location = new System.Drawing.Point(0, 0);
+            this.lbl_name2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_name2.Name = "lbl_name2";
+            this.lbl_name2.Size = new System.Drawing.Size(90, 38);
+            this.lbl_name2.TabIndex = 1;
+            this.lbl_name2.Text = "Client";
+            this.lbl_name2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnl_player1
+            // 
+            this.pnl_player1.BackColor = System.Drawing.Color.Transparent;
+            this.pnl_player1.Controls.Add(this.pbox_player1);
+            this.pnl_player1.Controls.Add(this.pbox_health1);
+            this.pnl_player1.Controls.Add(this.lbl_name1);
+            this.pnl_player1.Location = new System.Drawing.Point(90, 307);
+            this.pnl_player1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnl_player1.Name = "pnl_player1";
+            this.pnl_player1.Size = new System.Drawing.Size(90, 154);
+            this.pnl_player1.TabIndex = 3;
+            // 
+            // pbox_player1
+            // 
+            this.pbox_player1.Location = new System.Drawing.Point(0, 62);
+            this.pbox_player1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_player1.Name = "pbox_player1";
+            this.pbox_player1.Size = new System.Drawing.Size(90, 92);
+            this.pbox_player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox_player1.TabIndex = 3;
+            this.pbox_player1.TabStop = false;
+            // 
+            // pbox_health1
+            // 
+            this.pbox_health1.Location = new System.Drawing.Point(0, 38);
+            this.pbox_health1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_health1.Name = "pbox_health1";
+            this.pbox_health1.Size = new System.Drawing.Size(90, 23);
+            this.pbox_health1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbox_health1.TabIndex = 1;
+            this.pbox_health1.TabStop = false;
+            // 
+            // lbl_name1
+            // 
+            this.lbl_name1.Font = new System.Drawing.Font("Berlin Sans FB", 9F, System.Drawing.FontStyle.Bold);
+            this.lbl_name1.ForeColor = System.Drawing.Color.Black;
+            this.lbl_name1.Location = new System.Drawing.Point(0, 0);
+            this.lbl_name1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_name1.Name = "lbl_name1";
+            this.lbl_name1.Size = new System.Drawing.Size(90, 38);
+            this.lbl_name1.TabIndex = 1;
+            this.lbl_name1.Text = "Server";
+            this.lbl_name1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm_play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.pnl_player2);
+            this.Controls.Add(this.pnl_player1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_play";
+            this.Text = "na";
+            this.Load += new System.EventHandler(this.frm_play_Load);
+            this.pnl_player2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_health2)).EndInit();
+            this.pnl_player1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_health1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Panel pnl_player2;
+        internal System.Windows.Forms.PictureBox pbox_player2;
+        internal System.Windows.Forms.PictureBox pbox_health2;
+        internal System.Windows.Forms.Label lbl_name2;
+        internal System.Windows.Forms.Panel pnl_player1;
+        internal System.Windows.Forms.PictureBox pbox_player1;
+        internal System.Windows.Forms.PictureBox pbox_health1;
+        internal System.Windows.Forms.Label lbl_name1;
+    }
+}
+
---
+++ BattleFood/BattleFood/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleFood
+{
+    public partial class frm_play : Form
+    {
+        public cls_network networkConnection = new cls_network;
+        private String result = "";
+        public Boolean isServer = false;
+
+        private int positionXinMap = 0;
+        private Image map_background;
+
+        private enum Team
+        {
+            Player1,
+            Player2
+        }
+
+        private enum PlayerImage
+        {
+            Potato,
+            Melone,
+            Onion
+        }
+
+        private Team currentPlayer;
+        private PlayerImage currentPlayerImage;
+        private Boolean initSend = true;
+
+        private PlayerImage player1_image = PlayerImage.Potato;
+        private Boolean player1_laufeRechts = true;
+        private int player1_health = 100;
+        private Point player1_position;
+        private int player1_curRunPos = 0;
+        private Boolean player1_jump = false;
+
+        private PlayerImage player2_image = PlayerImage.Potato;
+        private Boolean player2_laufeRechts = true;
+        private int player2_health = 100;
+        private Point player2_position;
+        private int player2_curRunPos = 0;
+        private Boolean player2_jump = false;
+
+        private Boolean schuss1_nachRechts = true;
+        private Boolean schuss2_nachRechts = true;
+
+        private Image[] p_laufenImage = new Image[5];
+        private Image[] p_laufenImageI = new Image[5];
+        private Image[] m_laufenImage = new Image[5];
+        private Image[] m_laufenImageI = new Image[5];
+        private Image[] o_laufenImage = new Image[5];
+        private Image[] o_laufenImageI = new Image[5];
+
+        // Variablen für die fallenden Objekte:
+        private int PositionX, PositionY;
+        private Queue picbox = new Queue();
+        private int frequency = 1;
+        private Random rnd = new Random();
+
+        public frm_play()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_play_Load(object sender, EventArgs e)
+        {
+            InitialImages();
+
+        }
+
+        private void InitialImages()
+        {
+            map_background = Properties.Resources.de_kitchen2_final;
+            BackgroundImage = map_background;
+
+
+        }
+    }
+}
