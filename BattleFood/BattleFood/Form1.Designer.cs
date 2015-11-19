313fd659ccdac31ee042d5ea839f49751bcce802
makroground <r.bressler@makroground.de>
2015-11-19 21:22:07 +0100

    Zusammengeführt

--- BattleFood/BattleFood/Form1.Designer.cs
+++ BattleFood/BattleFood/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnl_player2 = new System.Windows.Forms.Panel();
             this.pbox_player2 = new System.Windows.Forms.PictureBox();
             this.pbox_health2 = new System.Windows.Forms.PictureBox();
@@ -36,12 +37,22 @@
             this.pbox_player1 = new System.Windows.Forms.PictureBox();
             this.pbox_health1 = new System.Windows.Forms.PictureBox();
             this.lbl_name1 = new System.Windows.Forms.Label();
+            this.tmr_animateImage = new System.Windows.Forms.Timer(this.components);
+            this.tmr_schiessen1 = new System.Windows.Forms.Timer(this.components);
+            this.pbox_kugel1 = new System.Windows.Forms.PictureBox();
+            this.pbox_kugel2 = new System.Windows.Forms.PictureBox();
+            this.tmr_schiessen2 = new System.Windows.Forms.Timer(this.components);
+            this.tmr_jump = new System.Windows.Forms.Timer(this.components);
+            this.tmr_falldown1 = new System.Windows.Forms.Timer(this.components);
+            this.tmr_falldown2 = new System.Windows.Forms.Timer(this.components);
             this.pnl_player2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_player2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_health2)).BeginInit();
             this.pnl_player1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_player1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_health1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_kugel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_kugel2)).BeginInit();
             this.SuspendLayout();
             // 
             // pnl_player2
@@ -132,24 +143,79 @@
             this.lbl_name1.Text = "Server";
             this.lbl_name1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // tmr_animateImage
+            // 
+            this.tmr_animateImage.Interval = 20;
+            this.tmr_animateImage.Tick += new System.EventHandler(this.tmr_animateImage_Tick);
+            // 
+            // tmr_schiessen1
+            // 
+            this.tmr_schiessen1.Interval = 10;
+            this.tmr_schiessen1.Tick += new System.EventHandler(this.tmr_schiessen1_Tick);
+            // 
+            // pbox_kugel1
+            // 
+            this.pbox_kugel1.BackColor = System.Drawing.Color.Transparent;
+            this.pbox_kugel1.Location = new System.Drawing.Point(-30, -15);
+            this.pbox_kugel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_kugel1.Name = "pbox_kugel1";
+            this.pbox_kugel1.Size = new System.Drawing.Size(30, 15);
+            this.pbox_kugel1.TabIndex = 5;
+            this.pbox_kugel1.TabStop = false;
+            // 
+            // pbox_kugel2
+            // 
+            this.pbox_kugel2.BackColor = System.Drawing.Color.Transparent;
+            this.pbox_kugel2.Location = new System.Drawing.Point(-30, -15);
+            this.pbox_kugel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbox_kugel2.Name = "pbox_kugel2";
+            this.pbox_kugel2.Size = new System.Drawing.Size(30, 15);
+            this.pbox_kugel2.TabIndex = 6;
+            this.pbox_kugel2.TabStop = false;
+            // 
+            // tmr_schiessen2
+            // 
+            this.tmr_schiessen2.Interval = 10;
+            this.tmr_schiessen2.Tick += new System.EventHandler(this.tmr_schiessen2_Tick);
+            // 
+            // tmr_jump
+            // 
+            this.tmr_jump.Interval = 50;
+            this.tmr_jump.Tick += new System.EventHandler(this.tmr_jump_Tick);
+            // 
+            // tmr_falldown1
+            // 
+            this.tmr_falldown1.Interval = 25;
+            this.tmr_falldown1.Tick += new System.EventHandler(this.tmr_falldown1_Tick);
+            // 
+            // tmr_falldown2
+            // 
+            this.tmr_falldown2.Interval = 25;
+            this.tmr_falldown2.Tick += new System.EventHandler(this.tmr_falldown2_Tick);
+            // 
             // frm_play
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.pbox_kugel2);
+            this.Controls.Add(this.pbox_kugel1);
             this.Controls.Add(this.pnl_player2);
             this.Controls.Add(this.pnl_player1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_play";
             this.Text = "na";
             this.Load += new System.EventHandler(this.frm_play_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frm_play_KeyDown);
             this.pnl_player2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbox_player2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_health2)).EndInit();
             this.pnl_player1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbox_player1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbox_health1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_kugel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbox_kugel2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -164,6 +230,14 @@
         internal System.Windows.Forms.PictureBox pbox_player1;
         internal System.Windows.Forms.PictureBox pbox_health1;
         internal System.Windows.Forms.Label lbl_name1;
+        private System.Windows.Forms.Timer tmr_animateImage;
+        private System.Windows.Forms.Timer tmr_schiessen1;
+        internal System.Windows.Forms.PictureBox pbox_kugel1;
+        internal System.Windows.Forms.PictureBox pbox_kugel2;
+        private System.Windows.Forms.Timer tmr_schiessen2;
+        private System.Windows.Forms.Timer tmr_jump;
+        private System.Windows.Forms.Timer tmr_falldown1;
+        private System.Windows.Forms.Timer tmr_falldown2;
     }
 }
 
--- BattleFood/BattleFood/Form1.cs
+++ BattleFood/BattleFood/Form1.cs
@@ -82,7 +82,253 @@ namespace BattleFood
             map_background = Properties.Resources.de_kitchen2_final;
             BackgroundImage = map_background;
 
+            player1_position = new Point(150, ClientSize.Height - pnl_player1.Height - 180);
+            pnl_player1.Location = player1_position;
+            switch (player1_image)
+            {
+                case PlayerImage.Potato:
+                    pbox_player1.Image = spiegelImage(Properties.Resources.p_normal);
+                    break;
+                case PlayerImage.Melone:
+                    pbox_player1.Image = spiegelImage(Properties.Resources.m_normal);
+                    break;
+                case PlayerImage.Onion:
+                    pbox_player1.Image = spiegelImage(Properties.Resources.o_normal);
+                    break;
+            }
+
+            player2_position = new Point(ClientSize.Width - pnl_player2.Width - 150, ClientSize.Height - pnl_player2.Height - 180);
+            pnl_player2.Location = player2_position;
+            switch (player2_image)
+            {
+                case PlayerImage.Potato:
+                    pbox_player2.Image = spiegelImage(Properties.Resources.p_normal);
+                    break;
+                case PlayerImage.Melone:
+                    pbox_player2.Image = spiegelImage(Properties.Resources.m_normal);
+                    break;
+                case PlayerImage.Onion:
+                    pbox_player2.Image = spiegelImage(Properties.Resources.o_normal);
+                    break;
+            }
+
+            p_laufenImage[0] = (Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 0);
+            p_laufenImage[1] = (Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 1);
+            p_laufenImage[2] = (Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 2);
+            p_laufenImage[3] = (Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 3);
+            p_laufenImage[4] = (Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 4);
+
+            p_laufenImage[0] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 0));
+            p_laufenImage[1] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 1));
+            p_laufenImage[2] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 2));
+            p_laufenImage[3] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 3));
+            p_laufenImage[4] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("p_lfn_" + 4));
+
+            p_laufenImage[0] = (Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 0);
+            p_laufenImage[1] = (Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 1);
+            p_laufenImage[2] = (Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 2);
+            p_laufenImage[3] = (Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 3);
+            p_laufenImage[4] = (Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 4);
+
+            p_laufenImage[0] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 0));
+            p_laufenImage[1] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 1));
+            p_laufenImage[2] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 2));
+            p_laufenImage[3] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 3));
+            p_laufenImage[4] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("m_lfn_" + 4));
+
+            p_laufenImage[0] = (Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 0);
+            p_laufenImage[1] = (Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 1);
+            p_laufenImage[2] = (Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 2);
+            p_laufenImage[3] = (Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 3);
+            p_laufenImage[4] = (Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 4);
+
+            p_laufenImage[0] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 0));
+            p_laufenImage[1] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 1));
+            p_laufenImage[2] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 2));
+            p_laufenImage[3] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 3));
+            p_laufenImage[4] = spiegelImage((Image)Properties.Resources.ResourceManager.GetObject("o_lfn_" + 4));
+        }
+
+        private void spiegelPictureBox(PictureBox pbox)
+        {
+            Image image = pbox.Image;
+            image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            pbox.Image = image;
+        }
+
+        private void frm_play_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    switch (currentPlayer)
+                    {
+                        case Team.Player1:
+                            if (player1_health > 0)
+                            {
+                                player1_laufeRechts = true;
+                                tmr_animateImage.Start();
+                            }
+                            break;
+                        case Team.Player2:
+                            if (player2_health > 0)
+                            {
+                                player2_laufeRechts = true;
+                                tmr_animateImage.Start();
+                            }
+                            break;
+                    }
+                    break;
+                case Keys.Left:
+                    switch (currentPlayer)
+                    {
+                        case Team.Player1:
+                            if (player1_health > 0)
+                            {
+                                player1_laufeRechts = false;
+                                tmr_animateImage.Start();
+                            }
+                            break;
+                        case Team.Player2:
+                            if (player2_health > 0)
+                            {
+                                player2_laufeRechts = false;
+                                tmr_animateImage.Start();
+                            }
+                            break;
+                    }
+                    break;
+                case Keys.Escape:
+                    frm_options.Show();
+                    break;
+                case Keys.Space:
+                    switch (currentPlayer)
+                    {
+                        case Team.Player1:
+                            if (tmr_schiessen1.Enabled == false)
+                            {
+                                schuss1_nachRechts = true;
+                                pbox_kugel1.Location = new Point(pnl_player1.Location.X + 70, pnl_player1.Location.Y + 80);
+                                pbox_kugel1.Image = Properties.Resources.bulletrechts;
+                            }
+                            else
+                            {
+                                schuss1_nachRechts = false;
+                                pbox_kugel1.Location = new Point(pnl_player1.Location.X - 30, pnl_player1.Location.Y + 80);
+                                pbox_kugel1.Image = spiegelImage(Properties.Resources.bulletrechts);
+                            }
+                            tmr_schiessen1.Start();
+                            break;
+                        case Team.Player2:
+                            if (tmr_schiessen2.Enabled == false)
+                            {
+                                schuss2_nachRechts = true;
+                                pbox_kugel2.Location = new Point(pnl_player2.Location.X + 70, pnl_player2.Location.Y + 80);
+                                pbox_kugel2.Image = Properties.Resources.bulletrechts;
+                            }
+                            else
+                            {
+                                schuss2_nachRechts = false;
+                                pbox_kugel2.Location = new Point(pnl_player2.Location.X - 30, pnl_player2.Location.Y + 80);
+                                pbox_kugel2.Image = spiegelImage(Properties.Resources.bulletrechts);
+                            }
+                            tmr_schiessen2.Start();
+                            break;
+                    }
+                    break;
+                case Keys.Up:
+                    switch (currentPlayer)
+                    {
+                        case Team.Player1:
+                            if (tmr_jump.Enabled == false)
+                            {
+                                player1_jump = true;
+                                tmr_jump.Start();
+                            }
+                            break;
+                        case Team.Player2:
+                            if (tmr_jump.Enabled == false)
+                            {
+                                player2_jump = true;
+                                tmr_jump.Start();
+                            }
+                            break;
+                    }
+                    break;
+            }
+
+            switch (currentPlayer)
+            {
+                case Team.Player1:
+                    if (IsOutOfTable(pnl_player1))
+                    {
+                        if (tmr_animateImage.Enabled)
+                        {
+                            tmr_animateImage.Stop();
+                        }
+                        if (tmr_falldown1.Enabled = false)
+                        {
+                            player1_health = 0;
+                            SetzeLebensAnzeige(pbox_health1, pbox_player1, player1_health, player1_image);
+                            tmr_falldown1.Start();
+                        }
+                    }
+                    break;
+                case Team.Player2
+                    if (IsOutOfTable(pnl_player2))
+                    {
+                        if (tmr_animateImage.Enabled)
+                        {
+                            tmr_animateImage.Stop();
+                        }
+                        if (tmr_falldown2.Enabled = false)
+                        {
+                            player2_health = 0;
+                            SetzeLebensAnzeige(pbox_health2, pbox_player2, player2_health, player2_image);
+                            tmr_falldown2.Start();
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void tmr_animateImage_Tick(object sender, EventArgs e)
+        {
 
         }
+
+        private void tmr_schiessen1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_schiessen2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_jump_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_falldown1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_falldown2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private Image spiegelImage(Image img)
+        {
+            Image image = img;
+            image.RotateFlip(RotateFlipType.Rotate180FlipY);
+            return image;
+        }
+
+
     }
 }
